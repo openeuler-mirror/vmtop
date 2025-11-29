db9ae8026edc2fec6a7b31257aa58adca6017042
lizixin <2271170409@qq.com>
2025-11-29 15:55:55 +0800

    System hardening: When host memory is insufficient, the return value of malloc must be validated. If memory allocation fails, the program should terminate with an appropriate error code.

--- src/vmtop.c
+++ src/vmtop.c
@@ -459,6 +459,9 @@ int main(int argc, char *argv[])
     int key;
 
     init_parameter();
+    if (!vcpu_list.domains) {
+        return -1;
+    }
     parse_args(argc, argv);
     if (scr_mode == TERM_MODE) {
         print_scr = printw;
